0faab70699827cee0de5e1ecdb783bf5ca6aedc9
RoshRoslin <RoshRoslin@gmail.com>
2021-05-08 16:57:53 -0700

    Updates

---
+++ precache-manifest.45584828cbf66aaafaa4fc32d504da02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08d1ebb4de6e39ad1e7a267464df693f",
+    "url": "/react-tabs-project/index.html"
+  },
+  {
+    "revision": "ab36bb8969bb0054d6d7",
+    "url": "/react-tabs-project/static/css/main.fd50c553.chunk.css"
+  },
+  {
+    "revision": "0bbdd4898da7f8fccfc5",
+    "url": "/react-tabs-project/static/js/2.8a5553c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tabs-project/static/js/2.8a5553c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab36bb8969bb0054d6d7",
+    "url": "/react-tabs-project/static/js/main.48240ced.chunk.js"
+  },
+  {
+    "revision": "6635115a8e0deb5a7024",
+    "url": "/react-tabs-project/static/js/runtime-main.633b60dc.js"
+  }
+]);
