15373ae58a1c9688227eb2f59e21abd7f2d6fc11
RoshRoslin <RoshRoslin@gmail.com>
2021-05-08 16:58:43 -0700

    First commit, source code

---
+++ src/data.js
@@ -0,0 +1,41 @@
+const data = [
+  {
+    id: 'recAGJfiU4CeaV0HL',
+    order: 3,
+    title: 'Full Stack Web Developer',
+    dates: 'December 2015 - Present',
+    duties: [
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam alias cupiditate nulla impedit mollitia officiis dolorum odit eaque! Earum?',
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis in totam ab unde neque est aliquam nam, dignissimos sed illo?',
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab praesentium aspernatur deleniti eaque, at atque unde accusantium eum quas laudantium?',
+    ],
+    company: 'TOMMY',
+  },
+  {
+    id: 'recIL6mJNfWObonls',
+    order: 2,
+    title: 'Front-End Engineer',
+    dates: 'May 2015 - December 2015',
+    duties: [
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto, fugiat saepe. Suscipit soluta commodi quas laborum molestias, in rerum voluptatum impedit exercitationem quibusdam architecto facere id provident blanditiis dolorem quae!',
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias minima, maiores sunt dolores ducimus saepe tenetur quibusdam consequuntur veniam consectetur officia voluptas error deserunt molestiae? Culpa dolores velit quasi nobis, debitis illo dicta iure soluta natus beatae, laudantium quos iusto!',
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque fugiat nemo quas illum dolorem repellendus!',
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis dolores, accusantium ullam necessitatibus libero aliquam dolor impedit autem nisi, voluptas id provident reprehenderit officia consectetur!',
+    ],
+    company: 'BIGDROP',
+  },
+  {
+    id: 'rec61x18GVY99hQq5',
+    order: 1,
+    title: 'Engineering Intern',
+    dates: 'May 2014 - September 2015',
+    duties: [
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla culpa repellendus esse necessitatibus quos id, quod voluptas iure velit officiis qui voluptate beatae omnis? Velit dolorum, tempora assumenda et culpa exercitationem! Non veniam saepe aliquam!',
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo sint sequi nobis atque molestias. Quibusdam saepe sapiente qui corporis accusantium tempore, ipsum nulla eius dolorem explicabo voluptates commodi similique.',
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit.  hic amet, doloremque neque reprehenderit excepturi repudiandae consectetur.',
+    ],
+    company: 'CUKER',
+  },
+];
+
+export default data;
